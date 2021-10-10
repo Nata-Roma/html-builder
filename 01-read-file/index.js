11a97ccdaa1fc86b885fd8969fc6a66b32747bf2
Natalia Romashko <sharkellgm@gmail>
2021-10-10 20:46:59 +0300

    fix: remove unnecessary code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { stdout, stdin } = require('process');
+const { stdout } = require('process');
 
 const filePath = path.join(__dirname, 'text.txt');
 
