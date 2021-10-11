2f8ca522654e41a3408156ed80f8236b1ecadfb8
Natalia Romashko <sharkellgm@gmail>
2021-10-12 02:34:34 +0300

    feat: add files copying

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,6 +5,7 @@ const fsPromises = require('fs/promises');
 const cssDir = path.join(__dirname, 'styles');
 const distDir = path.join(__dirname, 'project-dist');
 const componentDir = path.join(__dirname, 'components');
+const assetDir = path.join(__dirname, 'assets');
 
 const createCssBundle = async (distDir, cssDir) => {
   const files = await fsPromises.readdir(cssDir, { withFileTypes: true });
@@ -17,30 +18,61 @@ const createCssBundle = async (distDir, cssDir) => {
 }
 
 const createHtmlFile = async (distDir, componentDir) => {
-  const fileRes = await fsPromises.readFile(path.join(__dirname, 'template.html'), 'utf-8');
+  let fileRes = await fsPromises.readFile(path.join(__dirname, 'template.html'), 'utf-8');
   const files = await fsPromises.readdir(componentDir, { withFileTypes: true });
+  // console.log(files);
   files.forEach(async (file) => {
     if (path.extname(file.name) === '.html') {
       const component = await fsPromises.readFile(path.join(componentDir, file.name), 'utf-8');
       const index = file.name.lastIndexOf('.');
-      fileRes.replace(`{{${file.name.slice(0, index)}}}`, component)
+      const replaceable = `{{${file.name.slice(0, index)}}}`;
+      fileRes = fileRes.replace(replaceable, component);
       await fsPromises.writeFile(path.join(distDir, 'index.html'), fileRes);
     }
   })
-  // console.log(fileRes);
 }
 
-const createDir = async (distDir, cssDir, componentDir) => {
-  // const dirPath = path.join(__dirname, 'project-dist');
-  await fsPromises.mkdir(distDir).then(async () => {
-    await createCssBundle(distDir, cssDir);
-    await createHtmlFile(distDir, componentDir);
-    console.log('CREATE');
+const copyAssetsDir = async (distDir, assetDir) => {
+  const files = await fsPromises.readdir(assetDir, { withFileTypes: true });
+
+  files.forEach(async (file) => {
+    if (file.isDirectory()) {
+      const newDir = path.join(assetDir, file.name);
+      createDir(path.join(distDir, file.name))
+        .then(() => {
+          copyAssetsDir(path.join(distDir, file.name), newDir);
+        })
+        .catch(() => {
+          copyAssetsDir(path.join(distDir, file.name), newDir);
+        });
+    } else {
+      try {
+        await fsPromises.copyFile(path.join(assetDir, file.name), path.join(distDir, file.name));
+      } catch (error) {
+      }
+    }
+  })
+
+}
+
+const createAssetsDir = async (distDir, assetDir) => {
+  await fsPromises.mkdir(path.join(distDir, 'assets')).then(async () => {
+    await copyAssetsDir(path.join(distDir, 'assets'), assetDir);
   }).catch(async () => {
-    await createCssBundle(distDir, cssDir);
-    await createHtmlFile(distDir, componentDir);
-    console.log('EXISTS');
+    await copyAssetsDir(path.join(distDir, 'assets'), assetDir);
   })
 }
 
-createDir(distDir, cssDir, componentDir)
+const createDir = async (distDir, cssDir, componentDir, assetDir) => {
+  await fsPromises.mkdir(distDir).then(async () => {
+    await createCssBundle(distDir, cssDir);
+    await createAssetsDir(distDir, assetDir);
+    await createHtmlFile(distDir, componentDir);
+  }).catch(async () => {
+    await createCssBundle(distDir, cssDir);
+    await createAssetsDir(distDir, assetDir);
+    await createHtmlFile(distDir, componentDir);
+  })
+}
+
+createDir(distDir, cssDir, componentDir, assetDir)
